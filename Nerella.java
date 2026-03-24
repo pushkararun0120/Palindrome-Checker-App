3bb0b4480792c8f2768cf474b3779259d612e4e1
Pushkar Arun <arunpushkar0120@gmail.com>
2026-03-24 15:02:05 +0530

    Update Nerella.java

--- Nerella.java
+++ Nerella.java
@@ -1,5 +1,4 @@
-public class UseCase1PalindromeCheckerApp {
-
+public class UseCase3PalindromeCheckerApp {
     public static void main(String[] args) {
 
         // Display welcome message
@@ -12,6 +11,7 @@ public class UseCase1PalindromeCheckerApp {
         // Flow continues (next use case or exit)
         System.out.println("Application is ready to proceed...");
 
+        // UC2: Hardcoded Palindrome Check
         String input = "madam";
 
         System.out.println("Input text: " + input);
@@ -26,5 +26,19 @@ public class UseCase1PalindromeCheckerApp {
         }
 
         System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        // UC3: Palindrome Check Using String Reverse
+        String reversed = "";
+
+        // Iterate from the last character to the first
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        // Compare original and reversed using equals()
+        boolean isReversePalindrome = input.equals(reversed);
+
+        System.out.println("Reversed text: " + reversed);
+        System.out.println("Is it a Palindrome (Reverse Check)? : " + isReversePalindrome);
     }
 }
