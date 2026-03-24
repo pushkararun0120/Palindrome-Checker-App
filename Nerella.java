019212eb754a0340b63b97331003b28a52781fd8
Pushkar Arun <arunpushkar0120@gmail.com>
2026-03-24 14:22:29 +0530

    Create Palindrome Checker Application class

---
+++ Nerella.java
@@ -0,0 +1,15 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("Palindrome Checker App");
+
+        // Display application details
+        System.out.println("Version: 1.0");
+        System.out.println("Welcome to the Palindrome Checker Application!");
+
+        // Flow continues (next use case or exit)
+        System.out.println("Application is ready to proceed...");
+    }
+}
