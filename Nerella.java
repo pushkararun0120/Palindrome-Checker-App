a8f601447eb490935eae8b0839bf33c3a3b02453
Pushkar Arun <arunpushkar0120@gmail.com>
2026-03-24 14:45:13 +0530

    Add palindrome checking functionality

--- Nerella.java
+++ Nerella.java
@@ -11,5 +11,20 @@ public class UseCase1PalindromeCheckerApp {
 
         // Flow continues (next use case or exit)
         System.out.println("Application is ready to proceed...");
+
+        String input = "madam";
+
+        System.out.println("Input text: " + input);
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
